9d56fd6777a2d5c644941af2bd237713b5def3ef
hina kobayashi <snyew1760@gmail.com>
2020-11-16 15:46:38 +0900

    markupv1.0-mypage-point

---
+++ mypage/point.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+  <title>ポイント追加 | トルもんランド</title>
+  <meta name="title" content="トルモンランド">
+  <meta name="description" content="">
+  <meta name=" keywords" content="">
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="トルもんランド" />
+  <meta property="og:description" content="" />
+  <meta property="og:image" content="./assets/imgs/og-image.jpg" />
+  <meta property="og:url" content="" />
+  <meta property="og:site_name" content="トルもんランド" />
+  <link rel="shortcut icon" href="./favicon.ico">
+  <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png/">
+
+  <link href="../assets/css/common.css" rel="stylesheet">
+  <link href="../assets/css/reset.css" rel="stylesheet">
+  <link href="../assets/css/pages/point.css" rel="stylesheet" media="all">
+
+  <script src="../assets/js/jquery-3.4.1.min.js"></script>
+  <script src="../assets/js/main.js"></script>
+
+</head>
+
+<body>
+  <?php include(dirname(__FILE__).'../../header.php'); ?>
+
+  <main class="point-page">
+    <div class="container  flex">
+    <section class="sidebar">
+      <div class="container">
+        <ul class="pages">
+          <li class="page">
+            <a href="/mypage/index.php" class="link flex">
+              <img src="../assets/img/mypage/icon_top.svg" alt="" class="icon">
+              <p class="text">マイページトップ</p>
+            </a>
+          </li>
+
+          <li class="page">
+            <a href="/maypage/password.php" class="link flex">
+              <img src="../assets/img/mypage/icon_password.svg" alt="" class="icon">
+              <p class="text">パスワード変更</p>
+            </a>
+          </li>
+
+          <li class="page">
+            <a href="mypage/myitem.php" class="link flex">
+              <img src="../assets/img/mypage/icon_myitem.svg" alt="" class="icon">
+              <p class="text">獲得した景品&景品発送</p>
+            </a>
+          </li>
+
+          <li class="page">
+            <a href="/mypage/code.php" class="link flex">
+              <img src="../assets/img/mypage/icon_code.svg" alt="" class="icon">
+              <p class="text">各種コード入力</p>
+            </a>
+          </li>
+
+          <li class="page">
+            <a href="/mypage/withdrawal.php" class="link flex">
+              <img src="../assets/img/mypage/icon_withdrawal.svg" alt="" class="icon">
+              <p class="text">退会手続き</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+
+    <section class="section-content">
+      <div class="container">
+        <h1 class="title">ポイント追加</h1>
+        <div class="balance flex">
+          <p class="label">所持ポイント</p>
+          <p class="number">99,760</p>
+          <img src="../assets/img/point.svg" alt="ポイント" class="icon">
+        </div>
+
+        <ul class="points flex">
+          <li class="point">
+            <a href="/mypage/point-thanks.php" class="link">
+              <div class="number flex">
+                <p class="text">100</p>
+                <img src="../assets/img/point.svg" alt="ポイント" class="icon">
+              </div>
+              <p class="price">￥100</p>
+            </a>
+          </li>
+
+          <li class="point">
+            <a href="/mypage/point-thanks.php" class="link">
+              <div class="number flex">
+                <p class="text">300</p>
+                <img src="../assets/img/point.svg" alt="ポイント" class="icon">
+              </div>
+              <p class="price">￥300</p>
+            </a>
+          </li>
+
+          <li class="point">
+            <a href="/mypage/point-thanks.php" class="link">
+              <div class="number flex">
+                <p class="text">500</p>
+                <img src="../assets/img/point.svg" alt="ポイント" class="icon">
+              </div>
+              <p class="price">￥500</p>
+            </a>
+          </li>
+
+          <li class="point">
+            <a href="/mypage/point-thanks.php" class="link">
+              <div class="number flex">
+                <p class="text">1,000</p>
+                <img src="../assets/img/point.svg" alt="ポイント" class="icon">
+              </div>
+              <p class="price">￥1,000</p>
+            </a>
+          </li>
+          <li class="point">
+            <a href="/mypage/point-thanks.php" class="link">
+              <div class="number flex">
+                <p class="text">2,000</p>
+                <img src="../assets/img/point.svg" alt="ポイント" class="icon">
+              </div>
+              <p class="price">￥2,000</p>
+            </a>
+          </li>
+
+          <li class="point">
+            <a href="/mypage/point-thanks.php" class="link">
+              <div class="number flex">
+                <p class="text">3,000</p>
+                <img src="../assets/img/point.svg" alt="ポイント" class="icon">
+              </div>
+              <p class="price">￥3,000</p>
+            </a>
+          </li>
+
+          <li class="point">
+            <a href="/mypage/point-thanks.php" class="link">
+              <div class="number flex">
+                <p class="text">5,000</p>
+                <img src="../assets/img/point.svg" alt="ポイント" class="icon">
+              </div>
+              <p class="price">￥5,000</p>
+            </a>
+          </li>
+
+          <li class="point">
+            <a href="/mypage/point-thanks.php" class="link">
+              <div class="number flex">
+                <p class="text">10,000</p>
+                <img src="../assets/img/point.svg" alt="ポイント" class="icon">
+              </div>
+              <p class="price">￥10,000</p>
+            </a>
+          </li>
+          <li class="point">
+            <a href="/mypage/point-thanks.php" class="link">
+              <div class="number flex">
+                <p class="text">20,000</p>
+                <img src="../assets/img/point.svg" alt="ポイント" class="icon">
+              </div>
+              <p class="price">￥20,000</p>
+            </a>
+          </li>
+
+          <li class="point">
+            <a href="/mypage/point-thanks.php" class="link">
+              <div class="number flex">
+                <p class="text">30,000</p>
+                <img src="../assets/img/point.svg" alt="ポイント" class="icon">
+              </div>
+              <p class="price">￥30,000</p>
+            </a>
+          </li>
+        </ul>
+
+        <p class="transaction-law">
+          <a href="/privacy#tab02" class="link">特定商法取引の表示</a>
+        </p>
+        <p class="notice">※決済会社様の都合によりYahoo決済の場合一度の決済金額は¥21,800までとなっております。</p>
+      </div>
+
+    </section>
+    </div>
+  </main>
+  <?php include(dirname(__FILE__).'../../footer.php'); ?>
+</body>
+
+</html>
