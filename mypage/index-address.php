53a439d226901cd91d57a454921b3d5ad5b8f66e
hina kobayashi <snyew1760@gmail.com>
2020-12-19 02:10:35 +0900

    markupv1.0 -配送先編集スマホ用  / 修正 - 会員情報編集スマホ用

--- mypage/index-address.php
+++ mypage/index-address.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 
-  <title>会員情報編集| トルもんランド</title>
+  <title>景品配送先編集  | トルもんランド</title>
   <meta name="title" content="トルモンランド">
   <meta name="description" content="">
   <meta name=" keywords" content="">
@@ -21,7 +21,7 @@
 
   <link href="../assets/css/common.css" rel="stylesheet">
   <link href="../assets/css/reset.css" rel="stylesheet">
-  <link href="../assets/css/pages/mypage-edit.css" rel="stylesheet" media="all">
+  <link href="../assets/css/pages/mypage-address.css" rel="stylesheet" media="all">
 
   <script src="../assets/js/jquery-3.4.1.min.js"></script>
   <script src="../assets/js/main.js"></script>
@@ -31,74 +31,69 @@
 <body>
   <?php include ( dirname(__FILE__) . '/../header.php' ); ?>
 
-  <main class="mypage-edit-page">
+  <main class="mypage-address-page">
     <div class="container  flex">
+
       <section class="section-content">
         <div class="container">
           <form action="" class="form">
-            <h1 class="title">会員登録情報</h1>
+            <h2 class="title">景品配送先編集</h2>
+            <p class="text">基本情報の場所とは別の場所におくる際にご登録ください。</p>
             <table class="table">
-              <tr class="cell">
-                <th class="label">ニックネーム<span class="is-xs">(最大10文字)</span></th>
+            <tr class="cell read-area">
+                <th class="label">お名前</th>
                 <td class="data flex">
-                  <input class="input" type="text" name="name" value="">
-                  <button class="button" type="button">保存する</button>
+                <input class="input" type="tel" name="name" value="トルもん" readonly>
                 </td>
               </tr>
-              <tr class="cell">
-                <th class="label">名乗る称号</th>
-                <td class="data flex">
-                  <select class="select" id="status-select" name="status">
-                    <option value="pro">強者</option>
-                    <option value="beginner">初心者</option>
-                    <option value="dummy">ダミー</option>
-                  </select>
-                  <button class="button" type="button">保存する</button>
-                </td>
-              </tr>
-              <tr class="cell">
-                <th class="label">メールアドレス</th>
+              <tr class="cell read-area">
+                <th class="label">住所</th>
                 <td class="data">
-                  <input class="input" type="email" name="email" value="">
+                <span class="zip-icon">〒</span><input class="input zip" type="text" name="zip" value="107-0052"
+                    readonly>
+                  <input class="input" type="text" name="zip" value="東京都港区赤坂4-5-19" readonly>
+                </td>
+              </tr>
+              <tr class="cell read-area">
+                <th class="label">電話番号</th>
+                <td class="data">
+                <input class="input" type="tel" name="tel" value="03-3586-0411" readonly>
+                </td>
+              </tr>
+              <tr class="cell read-area">
+                <th class="label">性別</th>
+                <td class="data gender">
+                <p class="radio-text-gender man">男性</p>
+                  <p class="radio-text-gender woman">女性</p>
                 </td>
               </tr>
               <tr class="cell">
-                <th class="label">メール配信<span class="block">サービス</span></th>
-                <td class="data">
-                  <input class="radio" id="yes" type="radio" name="mail-magazine" value="yes">希望する
-                  <input class="radio" id="no" type="radio" name="mail-magazine" value="no" checked="checked">希望しない
-                </td>
-              </tr>
-              <tr class="cell">
-                <th class="label">SMS認証</th>
-                <td class="data">
-                  <p class="text">認証済み<br>※ゲームプレイ、課金、各種ボーナスポイントを受け取るためには認証を完了されている必要があります。</p>
+                <th class="label">編集</th>
+                <td class="data button">
+                <button class="button delete" type="submit">削除</button>
                 </td>
               </tr>
             </table>
-          </form>
 
-          <form action="" class="form">
-            <h2 class="title basic-info">基本情報</h2>
-            <button class="button-basechange" type="submit">保存する</button>
             <table class="table">
-              <tr class="cell">
+            <tr class="cell">
                 <th class="label">お名前</th>
                 <td class="data flex">
-                  <input class="input" type="text" name="name" value="">
+                <input class="input" type="tel" name="name" value="" placeholder="フルネームで">
                 </td>
               </tr>
               <tr class="cell">
                 <th class="label">住所</th>
                 <td class="data">
-                  〒<input class="input" type="text" name="zip" value="">
-                  <input class="input" type="text" name="zip" value="">
+                <span class="zip-icon">〒</span><input class="input zip" type="text" name="zip" value=""
+                    placeholder="1234567">
+                  <input class="input" type="text" name="zip" value="" placeholder="都道府県から">
                 </td>
               </tr>
               <tr class="cell">
                 <th class="label">電話番号</th>
                 <td class="data">
-                  <input class="input" type="tel" name="tel" value="">
+                <input class="input" type="tel" name="tel" value="" placeholder="000-0000-0000">
                 </td>
               </tr>
               <tr class="cell">
@@ -107,6 +102,12 @@
                 <label class="flex"><input type="radio" class="radio" id="man" name="gender" value="man" checked="checked"><p class="label">男性</p></label>
                 <label class="flex"><input type="radio" class="radio" id="woman" name="gender" value="woman"><p class="label">女性</p></label>
 
+                </td>
+              </tr>
+              <tr class="cell">
+                <th class="label">編集</th>
+                <td class="data button">
+                <button class="button add" type="submit">追加</button>
                 </td>
               </tr>
             </table>
